593324356ebd1131aafe6561a08c7ad2bdc9df5b
walkvoid <2916147177@qq.com>
2023-11-15 23:11:46 +0800

    TimePattern

---
+++ wvframework-models/src/main/java/com/wvframework/models/TimePattern.java
@@ -0,0 +1,30 @@
+package com.wvframework.models;
+
+/**
+ * @author walkvoid
+ * @desc time pattern
+ */
+public interface TimePattern {
+
+    /**
+     * yyyy-MM-dd HH:mm:ss
+     */
+    String P1 = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * yyyy-MM-dd
+     */
+    String P2 = "yyyy-MM-dd";
+
+    /**
+     * yyyy年MM月dd日 HH时mm分ss秒
+     */
+    String P3 = "yyyy年MM月dd日 HH时mm分ss秒";
+
+    /**
+     * "yyyy年MM月dd日
+     */
+    String P4 = "yyyy年MM月dd日";
+
+
+}
