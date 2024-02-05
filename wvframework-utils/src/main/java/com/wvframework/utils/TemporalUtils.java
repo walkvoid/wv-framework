d4d4af3937ab4acab5ee08ec1f4f8ab8254505b3
walkvoid <2916147177@qq.com>
2024-02-05 18:03:20 +0800

    TemporalUtils一些方法定义

---
+++ wvframework-utils/src/main/java/com/wvframework/utils/TemporalUtils.java
@@ -0,0 +1,64 @@
+package com.wvframework.utils;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.temporal.Temporal;
+
+/**
+ * @author walkvoid
+ * @description: LocalDateTime,LocalDate,... utils
+ */
+public class TemporalUtils {
+
+    private TemporalUtils(){}
+
+
+    /**
+     * 解析一个时间字符串
+     * @param content
+     * @return
+     */
+    public static Temporal parse(String content) {
+        ZoneId zoneId = ZoneId.systemDefault();
+        ZoneOffset of = ZoneOffset.of(zoneId.getId());
+
+        return null;
+    }
+
+    /**
+     * 解析一个字符串,并可以指定期望的类型
+     * @param content
+     * @param except
+     * @param <T>
+     * @return
+     */
+    public static <T extends Temporal> T parse(String content, Class<T> except) {
+        return null;
+    }
+
+    /**
+     * 解析一个字符串,并可以指定日期格式和指定期望的类型
+     * @param content
+     * @param pattern
+     * @param except
+     * @param <T>
+     * @return
+     */
+    public static <T extends Temporal> T parse(String content, String pattern, Class<T> except) {
+        return null;
+    }
+
+    /**
+     * 解析一个字符串,并可以指定日期格式和指定期望的类型
+     * @param epochSecond
+     * @param except
+     * @param <T>
+     * @return
+     */
+    public static <T extends Temporal> T ofSeconds(long epochSecond, Class<T> except) {
+        return null;
+    }
+
+
+}
