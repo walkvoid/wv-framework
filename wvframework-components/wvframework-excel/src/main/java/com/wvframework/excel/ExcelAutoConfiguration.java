5973690fe3db10d10f7465377399cfb9474c2b3e
walkvoid <2916147177@qq.com>
2024-07-31 18:13:59 +0800

    导出组件注解定义

---
+++ wvframework-components/wvframework-excel/src/main/java/com/wvframework/excel/ExcelAutoConfiguration.java
@@ -0,0 +1,14 @@
+package com.wvframework.excel;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author jiangjunqing
+ * @date 2024/7/31
+ * @description:
+ * @version:
+ */
+public class ExcelAutoConfiguration {
+
+}
---
+++ wvframework-components/wvframework-excel/src/main/java/com/wvframework/excel/export/ExportRouteFilter.java
@@ -0,0 +1,11 @@
+package com.wvframework.excel.export;
+
+/**
+ * @author jiangjunqing
+ * @date 2024/7/31
+ * @description:
+ * @version:
+ */
+public class ExportRouteFilter {
+
+}
