52164ca7bf4a966488a10afadd405429aeb63bad
walkvoid <2916147177@qq.com>
2024-09-04 20:01:13 +0800

    时间转换工具类编写

--- wvframework-utils/src/main/java/com/wvframework/utils/TemporalUtils.java
+++ wvframework-utils/src/main/java/com/wvframework/utils/TemporalUtils.java
@@ -1,9 +1,8 @@
 package com.wvframework.utils;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
+import java.time.*;
 import java.time.temporal.Temporal;
+import java.util.Comparator;
 
 /**
  * @author walkvoid
@@ -36,6 +35,10 @@ public class TemporalUtils {
      * @return
      */
     public static <T extends Temporal> T parse(String content, Class<T> except) {
+
+
+
+
         return null;
     }
 
@@ -61,6 +64,100 @@ public class TemporalUtils {
     public static <T extends Temporal> T ofSeconds(long epochSecond, Class<T> except) {
         return null;
     }
+
+
+
+    interface Convertor<S,T> {
+
+        /**
+         * source convert to target
+         * @param source
+         * @return target
+         */
+        T convert(S source);
+
+
+    }
+
+    interface Convertor2<S,T> extends Convertor<S,T>{
+
+        /**
+         * source convert to target
+         * @param source
+         * @return target
+         */
+        T convert(S source, int m);
+
+        default T convert(S source) {
+            return convert(source, 1);
+        }
+    }
+
+    class LocalDateTimeToLocalDateConvertor implements Convertor<LocalDateTime, LocalDate> {
+
+        @Override
+        public LocalDate convert(LocalDateTime source) {
+            return source.toLocalDate();
+        }
+    }
+
+    class LocalDateTimeToLocalTimeConvertor implements Convertor<LocalDateTime, LocalTime> {
+
+        @Override
+        public LocalTime convert(LocalDateTime source) {
+            return null;
+        }
+    }
+
+    class LocalDateTimeToYearMonthConvertor implements Convertor<LocalDateTime, YearMonth> {
+
+        @Override
+        public YearMonth convert(LocalDateTime source) {
+            return null;
+        }
+    }
+
+    class LocalDateTimeToYearConvertor implements Convertor<LocalDateTime, Year> {
+        @Override
+        public Year convert(LocalDateTime source) {
+            return Year.of(source.getYear());
+        }
+    }
+
+    class LocalDateToLocalDateTimeConvertor implements Convertor<LocalDate, LocalDateTime> {
+
+        @Override
+        public LocalDate convert(LocalDateTime source) {
+            return source.toLocalDate();
+        }
+    }
+
+    class LocalDateTimeToLocalTimeConvertor implements Convertor<LocalDate, LocalTime> {
+
+        @Override
+        public LocalTime convert(LocalDateTime source) {
+            return null;
+        }
+    }
+
+    class LocalDateTimeToYearMonthConvertor implements Convertor<LocalDate, YearMonth> {
+
+        @Override
+        public YearMonth convert(LocalDateTime source) {
+            return null;
+        }
+    }
+
+    class LocalDateTimeToYearConvertor implements Convertor<LocalDate, Year> {
+        @Override
+        public Year convert(LocalDateTime source) {
+            return Year.of(source.getYear());
+        }
+    }
+
+
+
+
 
 
 }
